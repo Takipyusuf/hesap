dc948cd8515025a107cc8c44cdc70aca198129b6
Takipyusuf <nostaljioyunlarr@gmail.com>
2026-05-18 23:54:27 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCx6Xqxe6qVugVJkjkDiw9QgVln_B-adbU",
   authDomain: "hesaptakip-b9159.firebaseapp.com",
@@ -8,6 +7,3 @@ const firebaseConfig = {
   appId: "1:312655912552:web:d033021544e2e410f3e5dd",
   measurementId: "G-KTWLB9JZ0"
 };
-
-// Config nesnesini dışa aktar (Projenizin okuyabilmesi için)
-export default firebaseConfig;
